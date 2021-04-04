5cf0a95a571370baaaa0afcfdc11c5250dbe5f4f
mathe <santiosmatheus@gmail.com>
2021-04-04 11:35:03 -0400

    master

--- src/components/PostCard/styles.js
+++ src/components/PostCard/styles.js
@@ -148,7 +148,8 @@ export const Actions = styled.div`
   padding: 0 10px;
   margin-top: 12px; */
   @media (max-width: 750px) {
-    margin-top: 30%
+    margin-top: 60%;
+    left: -10vh;
   }
 `;
 
